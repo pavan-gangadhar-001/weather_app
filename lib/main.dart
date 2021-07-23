896ffe9bea9078d5a215e780233bc0e59260a3bb
haveldarx <pavan.gangadhar.2001@gmail.com>
2021-07-23 13:04:25 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'ui_weathers.dart';
+
+void main() {
+   runApp(
+     new MaterialApp(
+       title: 'Weather ',
+       home: new weathers(),
+     )
+   );
+}
